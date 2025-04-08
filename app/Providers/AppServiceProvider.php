f1c4e73d6421fc78bbc53cf0a775c3eee0f6ed59
linh.vtn | apikey:7f55e619c14252ff44a2582ae2f287b4b540735a524a9b6726741310cd877397 <linh.vtn@fsofts.com>
2025-04-08 13:55:42 +0700

    save

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\LogoSetting;
 use App\Models\PusherSetting;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -28,31 +29,56 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        $generalSetting = GeneralSetting::first();
-        $logoSetting = LogoSetting::first();
-        $mailSetting = EmailConfiguration::first();
-        $pusherSetting = PusherSetting::first();
-        /** set time zone */
-        Config::set('app.timezone', $generalSetting->time_zone);
+    $generalSetting = null;
+    $logoSetting = null;
+    $mailSetting = null;
+    $pusherSetting = null;
 
-        /** Set Mail Config */
+    if (Schema::hasTable('general_settings')) {
+        $generalSetting = GeneralSetting::first();
+    }
+
+    if (Schema::hasTable('logo_settings')) {
+        $logoSetting = LogoSetting::first();
+    }
+
+    if (Schema::hasTable('email_configurations')) {
+        $mailSetting = EmailConfiguration::first();
+    }
+
+    if (Schema::hasTable('pusher_settings')) {
+        $pusherSetting = PusherSetting::first();
+    }
+
+    /** set time zone */
+    if ($generalSetting && $generalSetting->time_zone) {
+        Config::set('app.timezone', $generalSetting->time_zone);
+    }
+
+    /** Set Mail Config */
+    if ($mailSetting) {
         Config::set('mail.mailers.smtp.host', $mailSetting->host);
         Config::set('mail.mailers.smtp.port', $mailSetting->port);
         Config::set('mail.mailers.smtp.encryption', $mailSetting->encryption);
         Config::set('mail.mailers.smtp.username', $mailSetting->username);
         Config::set('mail.mailers.smtp.password', $mailSetting->password);
+    }
 
-        /** Set Broadcasting Config */
+    /** Set Broadcasting Config */
+    if ($pusherSetting) {
         Config::set('broadcasting.connections.pusher.key', $pusherSetting->pusher_key);
         Config::set('broadcasting.connections.pusher.secret', $pusherSetting->pusher_secret);
         Config::set('broadcasting.connections.pusher.app_id', $pusherSetting->pusher_app_id);
-        Config::set('broadcasting.connections.pusher.options.host', "api-".$pusherSetting->pusher_cluster.".pusher.com");
+        Config::set('broadcasting.connections.pusher.options.host', "api-" . $pusherSetting->pusher_cluster . ".pusher.com");
+    }
 
-
-
-        /** Share variable at all view */
-        View::composer('*', function($view) use ($generalSetting, $logoSetting, $pusherSetting){
-            $view->with(['settings' => $generalSetting, 'logoSetting' => $logoSetting, 'pusherSetting' => $pusherSetting]);
-        });
+    /** Share variable at all view */
+    View::composer('*', function ($view) use ($generalSetting, $logoSetting, $pusherSetting) {
+        $view->with([
+            'settings' => $generalSetting,
+            'logoSetting' => $logoSetting,
+            'pusherSetting' => $pusherSetting
+        ]);
+    });
     }
 }
